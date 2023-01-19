6f86225343c1519b6c8fb127aef02ecc7ad2f53e
Ross M. Lodge <ross@projectronin.com>
2023-01-18 17:18:49 -0800

    Upgrade plugin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,3 @@
-node {
-    download.set(true)
-    version.set("18.12.1")
-}
-
 plugins {
-    id("com.projectronin.rest.contract.support") version "1.0.0"
+    id("com.projectronin.rest.contract.support") version "1.1.0"
 }
